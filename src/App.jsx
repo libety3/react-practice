826b67bd8b5d511cd5be6eae5e7a51c6e0c1969a
libety3 <77701838+libety3@users.noreply.github.com>
2021-02-03 07:14:51 +0900

    react-basic-study

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import React from "react";
-
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 const App = () => {
-  const onClickButton = () => alert();
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
+  console.log("さいしょ");
+
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+
+  const onClickSwitchShowFlag = () => {
+    // console.log(faceShowFlag);
+
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <p style={contentStyle}>お元気ですか？</p>
-      <button onClick={onClickButton}>ボタン</button>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>( *´艸｀)</p>}
     </>
   );
 };
